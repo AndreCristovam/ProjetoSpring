aeb9985f46d45a60294a54353668aadf04c3cede
AndreCristovam <35112115+AndreCristovam@users.noreply.github.com>
2021-11-26 16:42:14 -0300

    Alteração na coluna nome aluno

--- src/main/java/br/com/primeiroprojetospring/domain/Aluno.java
+++ src/main/java/br/com/primeiroprojetospring/domain/Aluno.java
@@ -3,6 +3,7 @@ package br.com.primeiroprojetospring.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,6 +23,7 @@ public class Aluno implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Column(name="ALUNO_NOME")
 	private String nome;
 
 	public Integer getId() {
