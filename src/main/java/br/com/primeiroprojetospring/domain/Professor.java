1b6bfbbb0f28d5c7addec3843442f5854ed9a1f2
andre cristovam <andrecristovam@gmail.com>
2021-11-26 16:30:44 -0300

    Adicionado anotação para coluna nome da entidade professor para NOME_PROFESSOR

--- src/main/java/br/com/primeiroprojetospring/domain/Professor.java
+++ src/main/java/br/com/primeiroprojetospring/domain/Professor.java
@@ -2,6 +2,7 @@ package br.com.primeiroprojetospring.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -17,6 +18,7 @@ public class Professor implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
+	@Column(name="NOME_PROFESSOR")
 	private String nome;
 
 	public Integer getId() {
