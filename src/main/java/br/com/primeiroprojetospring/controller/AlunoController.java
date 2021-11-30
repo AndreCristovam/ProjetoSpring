4884c8cca734c7f1bf08b3439a40e45f932ac45b
andre cristovam <andrecristovam@gmail.com>
2021-11-30 17:17:08 -0300

    Método altera e excluir aluno e professor

--- src/main/java/br/com/primeiroprojetospring/controller/AlunoController.java
+++ src/main/java/br/com/primeiroprojetospring/controller/AlunoController.java
@@ -3,6 +3,7 @@ package br.com.primeiroprojetospring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -34,6 +35,25 @@ public class AlunoController {
 	@PostMapping("/salvar")
 	public ModelAndView salvarAluno(Aluno aluno) {
 		alunoService.salvar(aluno);
+		return listaTodosAluno();
+	}
+	
+	@GetMapping("/alterar/{id}")
+	public ModelAndView alteraAluno(@PathVariable("id") Integer idAluno) {
+		ModelAndView mView = new ModelAndView("aluno/alteraAluno");
+		mView.addObject("aluno", alunoService.buscarAlunoID(idAluno));
+		return mView;
+	}
+	
+	@PostMapping("/alterar")
+	public ModelAndView alterar(Aluno alunoAlterado) {
+		alunoService.salvarAlteracao(alunoAlterado);
+		return listaTodosAluno();
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public ModelAndView excluir(@PathVariable("id") Integer id) {
+		alunoService.excluir(id);
 		return listaTodosAluno();
 	}
 
